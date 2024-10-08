adab688f4fe8647d88e73e02ec4153e7c1225279
Mujeeb Ur Rehman Shah <mujeebalishah147@gmail.com>
2024-10-08 16:08:52 +0500

    A Little Change in LAb 7

--- LABs/Lab_1/OddEvenSeries.java
+++ LABs/Lab_1/OddEvenSeries.java
@@ -22,8 +22,7 @@ public class OddEvenSeries {
                     System.out.print(arr[i]+" ");
                 }
             }
-
-            // this is any change 
+        
             for(int i = 0; i < arr.length; i++){
                 if (arr[i]%2 == 0){
                     System.out.print(arr[i]+" ");
--- LABs/Lab_7/Fabonacci.java
+++ LABs/Lab_7/Fabonacci.java
@@ -8,16 +8,30 @@ public class Fabonacci {
             return fab(n-1)+fab(n-2);
         }
     }
+ 
+    public static void fabitr(int n){
 
-//    public static int fabitr(int n){
-//        for (int i = 0; i < ; i++) {
-//
-//        }
-//    }
+        if (n == 0 || n == 1) {
+            System.out.println(n);
+        }
+        int num1 = 0;
+        int num2 = 1;
+        System.out.print(0+" ");
+        System.out.print(1+" ");
+        int num3;
+        for (int i = 2; i < n ; i++) {
+            num3 = num1 + num2;
+            System.out.print(num3+" ");
+            num1 = num2;
+            num2 = num3;
+
+        }
+    }
     public static void main(String[] args) {
         int n = 5;
         for (int i = 0; i < n; i++) {
             System.out.print(fab(i)+" ");
         }
+    fabitr(5);
     }
 }
